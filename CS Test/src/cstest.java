491f98e169a6d17e32ba9835f086c412ecd769ab
Noah <Noah@DESKTOP-R24N5E5.home>
2019-01-23 20:10:40 -0500

    Changed a comment

--- CS Test/src/cstest.java
+++ CS Test/src/cstest.java
@@ -2,7 +2,7 @@
 public class cstest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Changing this comment is like YES
 		System.out.println("This is a test of CS test");
 
 	}
