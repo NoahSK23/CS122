9710518e423a334c677ea6815e005a510381c467
klingern <klingern@P314-15.securebucks.edu>
2019-01-28 10:50:25 -0500

    Changed comment test

--- CS Test/src/cstest.java
+++ CS Test/src/cstest.java
@@ -3,7 +3,7 @@ public class cstest {
 
 	public static void main(String[] args) {
 		// Changing this comment is like YES
-		// Changing this from a different computer like HUH???
+		// Changing this from school...
 		System.out.println("This is a test of CS test");
 
 	}
